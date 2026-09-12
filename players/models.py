389682c4241038f4d121bd05dd80a76495adbf75
Guilherme Neves <guigarolim0@gmail.com>
2026-09-12 16:58:39 -0300

    feat: add Season and PlayerStats models

--- players/models.py
+++ players/models.py
@@ -37,3 +37,22 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+
+class Season(models.Model):
+    name = models.CharField(unique=True, max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class PlayerStats(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="stats")
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="stats")
+    goals = models.PositiveSmallIntegerField(default=0)
+    assists = models.PositiveSmallIntegerField(default=0)
+    matches_played = models.PositiveSmallIntegerField(default=0)
+    minutes_played = models.PositiveSmallIntegerField(default=0)
+    yellow_cards = models.PositiveSmallIntegerField(default=0)
+    red_cards = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.player.name} - {self.season.name}"
