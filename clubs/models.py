181db4b7654c4613a362c4a11f2bdff5fb0b67c4
Guilherme Neves <guigarolim0@gmail.com>
2026-09-11 23:44:43 -0300

    feat: add League, Club and Player models

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class League(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    tier = models.PositiveSmallIntegerField(help_text="1 for top division, 2 for second division, etc.")
+
+    def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    founded_year = models.PositiveIntegerField(null=True, blank=True)
+    stadium_name = models.CharField(max_length=150, null=True, blank=True)
+    crest_url = models.URLField(null=True, blank=True)
+    leagues = models.ManyToManyField(League, related_name="clubs")
+
+    def __str__(self):
+        return self.name
--- players/models.py
+++ players/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from clubs.models import Club
 
-# Create your models here.
+FOOT_CHOICES = [
+    ('R', 'Right'),
+    ('L', 'Left'),
+    ('B', 'Both'),
+]
+
+POSITION_CHOICES = [
+    ('GK', 'Goalkeeper'),
+    ('CB', 'Center Back'),
+    ('LB', 'Left Back'),
+    ('RB', 'Right Back'),
+    ('DM', 'Defensive Midfielder'),
+    ('CM', 'Central Midfielder'),
+    ('AM', 'Attacking Midfielder'),
+    ('LW', 'Left Winger'),
+    ('RW', 'Right Winger'),
+    ('ST', 'Striker'),
+]
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    height = models.FloatField()
+    weight = models.FloatField()
+    preferred_foot = models.CharField(max_length=1, choices=FOOT_CHOICES)
+    position = models.CharField(max_length=2, choices=POSITION_CHOICES)
+    current_club = models.ForeignKey(
+    Club,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name="players")
+
+    def __str__(self):
+        return self.name
