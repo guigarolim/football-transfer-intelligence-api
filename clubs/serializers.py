718f7440df4be1fd851bb057e7b165a02c6fcea2
Guilherme Neves <guigarolim0@gmail.com>
2026-09-12 22:27:34 -0300

    feat: add ClubSerializer with nested LeagueSerializer

--- clubs/serializers.py
+++ clubs/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import League
+from .models import Club
 
 class LeagueSerializer(serializers.ModelSerializer):
     class Meta:
         model = League
         fields = '__all__'
+
+class ClubSerializer(serializers.ModelSerializer):
+    leagues = LeagueSerializer(many=True, read_only=True)
+    class Meta:
+        model = Club
+        fields = '__all__'
+
