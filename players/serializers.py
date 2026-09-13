7f494c94aec6b5c4d6e6c1be18dfb2be3e00afb0
Guilherme Neves <guigarolim0@gmail.com>
2026-09-13 19:43:30 -0300

    feat: add SeasonSerializer

--- players/serializers.py
+++ players/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Player
 from clubs.serializers import ClubSerializer
+from .models import Season
 
 class PlayerSerializer(serializers.ModelSerializer):
     current_club = ClubSerializer(allow_null=True, read_only=True)
     class Meta:
         model = Player
         fields = '__all__'
+
+class SeasonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Season
+        fields = '__all__'
+        
